974b2ec799cf383d03336590a2f30dcaebaa6686
Ash <gaikwadashish2477@gmail.com>
2026-04-13 20:22:05 +0530

    fix(auth): auto-login user after successful signup

--- routes/user.js
+++ routes/user.js
@@ -13,9 +13,14 @@ router.post("/signup", async (req, res) => {
   try {
     let { username, email, password } = req.body;
     const newUser = new User({ email, username });
-    await User.register(newUser, password);
-    req.flash("success", "Welcome to Wanderlust!!");
-    res.redirect("/listings");
+    const registeredUser = await User.register(newUser, password);
+    req.login(registeredUser, (err) => {
+      if (err) {
+        return next(err);
+      }
+      req.flash("success", "Welcome to Wanderlust!!");
+      res.redirect("/listings");
+    });
   } catch (e) {
     req.flash("error", e.message);
     res.redirect("/signup");
@@ -32,23 +37,23 @@ router.post(
   "/login",
   passport.authenticate("local", {
     failureRedirect: "/login",
-    failureFlash: true
+    failureFlash: true,
   }),
   async (req, res) => {
     req.flash("success", "welcome back to Wanderlust");
-    res.redirect("/listings")
+    res.redirect("/listings");
   },
 );
 
 // GET /logout
 router.get("/logout", (req, res, next) => {
-    req.logout((err) => {
-        if (err) {
-            return next(err);
-        }
-        req.flash("success", "Logged Out!");
-        res.redirect("/listings");
-    })
-})
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    req.flash("success", "Logged Out!");
+    res.redirect("/listings");
+  });
+});
 
 module.exports = router;
