74236691439070375ab20c79af838448e0e489e9
Ash <gaikwadashish2477@gmail.com>
2026-04-15 09:21:13 +0530

    feat(reviews): refactor review routes to use controller methods for create and delete operations

--- routes/review.js
+++ routes/review.js
@@ -1,34 +1,16 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
-const Review = require("../models/review.js");
-const Listing = require("../models/listing.js");
 const {
   validateReview,
   isLoggedIn,
   isReviewAuthor,
 } = require("../middleware.js");
+const reviewController = require("../controllers/reviews.js");
 
 // reviews create route POST /listings/:id/reviews
-router.post("/", isLoggedIn, validateReview, async (req, res) => {
-  let { id } = req.params;
-  let listing = await Listing.findById(id);
-  let newReview = new Review(req.body.review);
-  newReview.author = req.user._id;
-  listing.reviews.push(newReview);
-  console.log(newReview);
-  await newReview.save();
-  await listing.save();
-  req.flash("success", "New Review Created!");
-  res.redirect(`/listings/${id}`);
-});
+router.post("/", isLoggedIn, validateReview, reviewController.createReview);
 
 // review delete route DELETE /listings/:listingId/reviews/:reviewId
-router.delete("/:reviewId", isLoggedIn, isReviewAuthor, async (req, res) => {
-  let { id, reviewId } = req.params;
-  await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-  await Review.findByIdAndDelete(reviewId);
-  req.flash("success", "Review Deleted!");
-  res.redirect(`/listings/${id}`);
-});
+router.delete("/:reviewId", isLoggedIn, isReviewAuthor, reviewController.destoryReview);
 
 module.exports = router;
