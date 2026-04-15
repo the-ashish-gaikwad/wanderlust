af45646a677eca94421c49eadc3f6bdf67439c7f
Ash <gaikwadashish2477@gmail.com>
2026-04-15 08:49:20 +0530

    feat(reviews): add isReviewAuthor middleware to authorize review deletion

--- middleware.js
+++ middleware.js
@@ -1,6 +1,7 @@
 const Listing = require("./models/listing.js");
 const ExpressError = require("./utils/expressError.js");
 const { listingSchema, reviewShcema } = require("./schema.js");
+const Review = require("./models/review.js");
 
 module.exports.isLoggedIn = (req, res, next) => {
       if (!req.isAuthenticated()) {
@@ -47,4 +48,14 @@ module.exports.validateReview = (req, res, next) => {
   } else {
     next();
   }
+};
+
+module.exports.isReviewAuthor = async (req, res, next) => {
+  let { reviewId, id } = req.params;
+  let review = await Review.findById(reviewId);
+  if (!review.author.equals(res.locals.currUser._id)) {
+    req.flash("error", "You don't have permission.");
+    return res.redirect(`/listings/${id}`);
+  }
+  next();
 };
--- routes/review.js
+++ routes/review.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const router = express.Router({mergeParams: true});
+const router = express.Router({ mergeParams: true });
 const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
-const { validateReview, isLoggedIn } = require("../middleware.js");
+const {
+  validateReview,
+  isLoggedIn,
+  isReviewAuthor,
+} = require("../middleware.js");
 
 // reviews create route POST /listings/:id/reviews
 router.post("/", isLoggedIn, validateReview, async (req, res) => {
@@ -19,9 +23,9 @@ router.post("/", isLoggedIn, validateReview, async (req, res) => {
 });
 
 // review delete route DELETE /listings/:listingId/reviews/:reviewId
-router.delete("/:reviewId", async(req, res) => {
-  let {id, reviewId} = req.params;
-  await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
+router.delete("/:reviewId", isLoggedIn, isReviewAuthor, async (req, res) => {
+  let { id, reviewId } = req.params;
+  await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
   await Review.findByIdAndDelete(reviewId);
   req.flash("success", "Review Deleted!");
   res.redirect(`/listings/${id}`);
