547cc2d8e3c1007c05a188918e8a903da3337eda
Ash <gaikwadashish2477@gmail.com>
2026-04-17 00:32:39 +0530

    feat(map): update map initialization to use listing coordinates and enhance marker popup

--- public/javascript/map.js
+++ public/javascript/map.js
@@ -3,10 +3,18 @@ maptilersdk.config.apiKey = mapKey;
 const map = new maptilersdk.Map({
   container: "map", // ID of the div
   style: maptilersdk.MapStyle.STREETS, // or other styles like SATELLITE, OUTDOORS, etc.
-  center: [coords[0], coords[1]], // longitude, latitude
+  center: [listing.geometry.coordinates[0], listing.geometry.coordinates[1]], // longitude, latitude
   zoom: 12, // starting zoom level
 });
 
 const redMarker = new maptilersdk.Marker({ color: "#ff385c" })
-  .setLngLat([coords[0], coords[1]])
+  .setLngLat([listing.geometry.coordinates[0], listing.geometry.coordinates[1]])
   .addTo(map);
+
+redMarker.setPopup(
+  new maptilersdk.Popup({ offset: 25 }).setHTML(
+    `<h3>${listing.title || "Location"}</h3>
+    <p>Exact location provided after booking.</p>
+    `,
+  ),
+);
