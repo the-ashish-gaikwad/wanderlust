f3eaacfcd5794d79c0a429737c22325f271ba7b9
Ash <gaikwadashish2477@gmail.com>
2026-04-17 00:19:52 +0530

    feat(map): update map initialization with dynamic coordinates and add marker

--- public/javascript/map.js
+++ public/javascript/map.js
@@ -3,6 +3,10 @@ maptilersdk.config.apiKey = mapKey;
 const map = new maptilersdk.Map({
   container: "map", // ID of the div
   style: maptilersdk.MapStyle.STREETS, // or other styles like SATELLITE, OUTDOORS, etc.
-  center: [77, 28], // longitude, latitude
-  zoom: 2, // starting zoom level
+  center: [coords[0], coords[1]], // longitude, latitude
+  zoom: 12, // starting zoom level
 });
+
+const redMarker = new maptilersdk.Marker({ color: "#ff385c" })
+  .setLngLat([coords[0], coords[1]])
+  .addTo(map);
